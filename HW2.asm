3a7ed70558d55f0adc3a60186dd29b07d6ea0e8a
AMA2581 <amir.abravesh.ama@gmail.com>
2024-01-05 17:11:50 +0330

    inital commit

---
+++ HW2.asm
@@ -0,0 +1,73 @@
+
+; You may customize this and other start-up templates; 
+; The location of this template is c:\emu8086\inc\0_com_template.txt
+
+org 100h
+
+.data 
+    array dw 10 dup(?) 
+    size dw 10
+    sum dw 0   
+    ten dw 10
+    msg1 db "Enter 10 Number: $" 
+    
+    
+    
+.code
+    main proc
+        mov ax , @data
+        mov ds , ax
+        call input_array
+        ret
+        main endp
+    
+    input_array proc
+        mov ah , 9
+        mov dx , offset msg1
+        int 21h
+        lea bx ,array
+        mov cx , size
+        push bx 
+        mov dx, 0
+        mov dl, 1  
+        mov bx , 0
+        
+        input_loop:
+            mov ah, 1
+            int 21h
+            cmp al, ' '
+            je proc_num
+            sub al, '0'
+            inc bl
+            mov ah, 0
+            push ax
+            jmp input_loop
+            
+            proc_num:  
+                cmp bl, 0
+                je next_num
+                pop ax
+                mul dl
+                add dh, al
+                mov al, dl
+                push dx
+                mul ten 
+                pop dx
+                mov dl, al 
+                dec bl
+                jmp proc_num 
+                   
+            next_num:
+                mov dx, 0
+                mov dl, 1 
+                mov bx, 0
+                
+                loop input_loop            
+        ret
+        input_array endp
+
+ret
+
+
+
+
